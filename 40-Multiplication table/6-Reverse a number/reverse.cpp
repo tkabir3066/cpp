e49442cc62ff270ad70d8cc6be790efbc2fed8a3
Tutul Kabir <tutulkabir@gmail.com>
2023-06-25 08:54:26 +0530

    changed in reverse a number

--- 40-Multiplication table/6-Reverse a number/reverse.cpp
+++ 40-Multiplication table/6-Reverse a number/reverse.cpp
@@ -1,28 +1,30 @@
-#include<iostream>
-#include<conio.h>
+#include <iostream>
+#include <conio.h>
 
 using namespace std;
 
-int main(){
+int main()
+{
 
-int num, r;
-cout<<"Enter an integer number : ";
-cin>>num;
+    int num, r;
+    cout << "Enter an integer number : ";
+    cin >> num;
 
-int sum=0;
-int temp = num;
+    int reversed_number = 0;
+    int temp = num;
 
-while(temp!=0){
-    r= temp%10;
-    sum= sum*10+r;
-    temp = temp/10;
-}
+    while (temp != 0)
+    {
+        r = temp % 10;
+        reversed_number = reversed_number * 10 + r;
+        temp = temp / 10;
+    }
 
-cout<<sum;
+    cout << reversed_number;
 
     getch();
 }
 
-//example:-
-//input-123
-// output-321
+// example:-
+// input-123
+//  output-321
